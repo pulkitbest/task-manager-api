b652a7cc22ad3907cd3409c4f8715d09baa8b77f
pulkitbest <pulkitgupta38@gmail.com>
2021-10-16 14:51:06 +0530

    file substructure changed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-const express = require('express')
-require('./db/mongoose')
+const app = require('./app')
 
-const User = require('./models/user')
-const Task = require('./models/task')
-
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-const { findByIdAndDelete } = require('./models/user')
-
-const app = express()
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, ()=>{
     console.log('Server is up on port ' + port)
